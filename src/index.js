115dfd1de220a4d447533d782013d49f00214f12
hataevicha5922 <hataevicha5922@gmail.com>
2021-03-12 07:04:59 +0300

    fix result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse(n) {
-    return +n.toString().split("").reverse().join("");
+    if (typeof n === "number") {
+        return Number(Math.abs(n).toString().split("").reverse().join(""));
+    }
 };
